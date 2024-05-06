7e160027e747ce0054aa1b312df4c2600365aacb
don <donaldolivergavino@gmail.com>
2024-05-06 10:39:53 +0800

    comment lots of thing

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
 const authRoute = require("../routes/authRoutes")
 require("dotenv").config();
 
@@ -15,9 +16,9 @@ mongoose
   .then(() => console.log("MongoDB is  connected successfully"))
   .catch((err) => console.error(err));
 
-app.listen(PORT, () => {
-    console.log(`Server is running on port: ${PORT}`);
-});
+// app.listen(PORT, () => {
+//     console.log(`Server is running on port: ${PORT}`);
+// });
 
 app.use(cors());
 
@@ -25,7 +26,7 @@ app.options("*", cors());
 
 
 app.use(cookieParser());
-
+// app.use(bodyParser.json());
 app.use(express.json());
 
 app.use("/", authRoute);
