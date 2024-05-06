4236a46e49056d96eca68f66de11331be41be1a8
don <donaldolivergavino@gmail.com>
2024-05-07 03:59:45 +0800

    forgot / in the route for getPhoto

--- routes/photoRoutes.js
+++ routes/photoRoutes.js
@@ -4,6 +4,6 @@ const { uploadPhoto, editPhoto, deletePhoto, getPhoto } = require("../controller
 router.post("/upload/photo", uploadPhoto);
 router.put("/edit/photo/:photoId", editPhoto);
 router.delete("/delete/photo/:photoId", deletePhoto);
-router.get("get/photo", getPhoto);
+router.get("/get/photo", getPhoto);
 
 module.exports = router;
